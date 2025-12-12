ce51558133c69b23415f671e1a77398d5f710dc0
Datta-B <90084638+Datta-B@users.noreply.github.com>
2025-12-12 16:55:58 +0530

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by dattaz biradar on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
